afb02132dcaf7aad1b227822526d657a75a823bd
Mathew Kleppin <matkle414@gmail.com>
2015-09-03 20:31:03 -0500

    add menu feature

---
+++ app/views/colors/colors.controller.js
@@ -0,0 +1,14 @@
+;(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('ColorsController', ColorsController);
+
+  ColorsController.$inject = [];
+
+  function ColorsController(){
+
+  }
+
+})();
